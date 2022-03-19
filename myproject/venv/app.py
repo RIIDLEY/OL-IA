99c130dbb2c95571b7f5757093a65b068bf82031
KryLex0 <adam.mkh@hotmail.fr>
2022-03-19 20:21:41 +0100

    Intégration du modèle dans la partie IHM
    Possibilité d'entrer plusieurs commentaires avec un retour du modèle.

--- myproject/venv/app.py
+++ myproject/venv/app.py
@@ -1,32 +1,44 @@
-from flask import Flask, jsonify, redirect, render_template, session,request, url_for
+import pathlib
+from flask import Flask, jsonify, redirect, render_template, session, request, url_for
 import subprocess as sp
-app = Flask(__name__)
+import pickle
 
-#https://pythonise.com/series/learning-flask/flask-working-with-forms
+from numpy import array
+app = Flask(__name__)
+filenameModel = '\model\\finalized_model.sav'
+filenameVectorizer = '\model\\vectorizer.sav'
+modelPath = str(pathlib.Path(__file__).parent)
+
+
+# https://pythonise.com/series/learning-flask/flask-working-with-forms
+
+@app.route('/')
+def index():
+   return redirect(url_for("comment_page"))
 
 @app.route('/comment_page', methods=["GET", "POST"])
 def comment_page():
+    if request.method == "POST":
+        requestData = request.form
+        session["requestData"] = requestData
+        return redirect(url_for("comment_page_result", requestData=requestData))
 
-   if request.method == "POST":
-      requestData = request.form
-      #print(requestData)
-      session["requestData"] = requestData
-      return redirect(url_for("comment_page_result", requestData = requestData))
-      #comment_page_result(request)
-      #return jsonify(req)
-   #comment_page_result()
-
-   return render_template("comment_page.html")
-
+    return render_template("comment_page.html")
 
 
 @app.route('/comment_page_result', methods=["GET", "POST"])
 def comment_page_result():
-   #comment_page()
-   if request.method == "POST":
-      requestData = request.form
-      print(requestData)
-      #session["requestData"] = requestData
-   #requestData = session.get("requestData")
-   #print(session)
-   return render_template("comment_page_result.html", requestData=requestData)
+    if request.method == "POST":
+        requestData = request.form
+
+        loaded_model = pickle.load(open(modelPath + filenameModel, 'rb'))
+        vectorizer = pickle.load(open(modelPath + filenameVectorizer, 'rb'))
+        commentResult = dict()
+        for i in requestData:
+            commentaire = [requestData.get(i)]
+            commentaire = vectorizer.transform(commentaire)
+            commentResult[requestData.get(
+                i)] = loaded_model.predict(commentaire)[0]
+
+        print(commentResult)
+    return render_template("comment_page_result.html", commentResult=commentResult)
---
+++ myproject/venv/project.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Étape 1. Importez les bibliothèques nécessaires, nous allons utiliser plus loin
+import urllib.request
+import os
+import pandas as pd
+import numpy as np
+import pickle
+from nltk.tokenize import RegexpTokenizer 
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+import pathlib
+filenameModel = '\model\\finalized_model.sav'
+filenameVectorizer = '\model\\vectorizer.sav'
+modelPath = str(pathlib.Path(__file__).parent)
+
+
+# In[2]:
+
+
+#Étape 2. Lecture du fichier .txt de Pandas et afficher les 3 premières lignes
+df = pd.read_table('dataset.txt', sep=';')
+df.head(3)
+
+
+# In[3]:
+
+
+#Étape 3. Nettoyer les données textuelles
+df.isnull().sum()
+
+
+# In[4]:
+
+
+#supprimer les valeurs null
+df = df.dropna()
+df.isnull().sum()
+
+
+# In[5]:
+
+
+#installer nltk 
+#import nltk
+#nltk.download()
+
+
+# In[6]:
+
+
+#tester nltk 
+#from nltk.corpus import brown
+#brown.words()
+
+
+# In[7]:
+
+
+# init Objects
+tokenizer = RegexpTokenizer(r'\w+')
+en_stopwords=set(stopwords.words('english'))
+ps=PorterStemmer()
+def getStemmedReview(review):
+    review=review.lower()
+    review=review.replace("<br /><br />"," ")
+    #Tokenize
+    tokens=tokenizer.tokenize(review)
+    new_tokens=[token for token in tokens if token not in  en_stopwords]
+    stemmed_tokens=[ps.stem(token) for token in new_tokens]
+    clean_review=' '.join(stemmed_tokens)
+    return clean_review
+
+
+# In[8]:
+
+
+#Étape 4. Nettoyer toutes les revues et diviser nos données pour la formation et les tests.
+df['review'].apply(getStemmedReview)
+X_train = df.loc[:8000, 'review'].values
+y_train = df.loc[:8000, 'sentiment'].values
+X_test = df.loc[2000:, 'review'].values
+y_test = df.loc[2000:, 'sentiment'].values
+
+
+# In[9]:
+
+
+#installer : pip3 install -U scikit-learn scipy matplotlib ou pip install -U scikit-learn scipy matplotlib
+
+
+# In[10]:
+
+
+#Étape 5. Transformer des mots en vecteurs de caractéristiques
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(sublinear_tf=True, encoding='utf-8',
+ decode_error='ignore')
+vectorizer.fit(X_train)
+X_train=vectorizer.transform(X_train)
+X_test=vectorizer.transform(X_test)
+
+
+# In[11]:
+
+
+#Étape 6. Création du modèle et vérification du score sur les données d'entraînement et de test
+from sklearn.linear_model import LogisticRegression
+
+model=LogisticRegression(solver='liblinear')
+model.fit(X_train,y_train)
+
+print("Score on training data is: "+str(model.score(X_train,y_train)))
+print("Score on testing data is: "+str(model.score(X_test,y_test)))
+
+
+def analyseComment(comment):
+    loaded_model = pickle.load(open(modelPath, 'rb'))
+    comment=vectorizer.transform(comment)
+
+    #retourne le résultat positif ou négatif
+    return loaded_model.predict(comment)
+
+
+
+#DUMP du model
+# save the model to disk
+pickle.dump(model, open(modelPath + filenameModel, 'wb'))
+
+pickle.dump(vectorizer, open(modelPath + filenameVectorizer, 'wb'))
+
+
+
+# In[12]:
+
+
+# tester sur une nouvelle valeur 
+#newval = ["While the diaper pins are attractive, the metal in the pins I received are flimsy and did not hold up to the thick fabric I used them on. Fortunately there was no baby involved"]
+
+#commentaire à analyser
+newval = ["I loved that movie. IN-CRE-DI-BLE!"]
+#print(analyseComment(newval))
+# In[13]:
+
+
+#newval=vectorizer.transform(newval)
+
+# In[14]:
+
+#retourne le résultat positif ou négatif
+#model.predict(newval)
+
+
+# In[15]:
+
+#pourcentage de positif/négatif
+#model.predict_proba(newval)
+
+
+# In[ ]:
+
+
+
+
